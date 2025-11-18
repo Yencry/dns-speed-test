8e6354669f5323a8a837f99213e3132664a0f208
Yencry <214315982+Yencry@users.noreply.github.com>
2025-11-18 20:45:19 +0800

    Update code

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/dnsSpeedTest/',  
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // 允许局域网访问
-    port: 4173, // 使用未占用的常规端口，避免与现有服务冲突
+    host: '0.0.0.0',
+    port: 4173,
     open: true
   }
 })
